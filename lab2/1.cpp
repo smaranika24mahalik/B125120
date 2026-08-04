b9d4a899a6b3588dd1b55bea10b5cdd4e9f3893f
smaranika24mahalik <b125120@iiit-bh.ac.in>
2026-08-04 22:46:36 +0530

    Create lab2/1.cpp

---
+++ lab2/1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+private:
+    int rollNo;
+    string name;
+    float marks;
+
+public:
+    void input()
+    {
+        cout << "Enter Roll Number: ";
+        cin >> rollNo;
+
+        cin.ignore();
+
+        cout << "Enter Name: ";
+        getline(cin, name);
+
+        cout << "Enter Marks: ";
+        cin >> marks;
+    }
+
+    void display()
+    {
+        cout << "\n----- Student Details -----\n";
+        cout << "Roll Number : " << rollNo << endl;
+        cout << "Name        : " << name << endl;
+        cout << "Marks       : " << marks << endl;
+    }
+};
+
+int main()
+{
+    Student s;
+
+    s.input();
+    s.display();
+
+    return 0;
+}
