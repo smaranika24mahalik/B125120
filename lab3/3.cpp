54f0123b2f16a7de3fd4ad42cb0993ff9249924c
smaranika24mahalik <b125120@iiit-bh.ac.in>
2026-08-11 21:45:12 +0530

    Create 3.cpp

---
+++ lab3/3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter size: ";
+    cin >> n;
+
+    int *arr = new int[n];
+
+    cout << "Enter elements: ";
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int largest = arr[0];
+
+    for(int i = 1; i < n; i++)
+    {
+        if(arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+
+    cout << "Largest element = " << largest << endl;
+
+    delete[] arr;
+
+    return 0;
+}
